1021fb88c15a8ad3f513ba14f14ff14e08b7b58f
unknown <robertrey1234@gmail.com>
2019-05-09 17:45:17 -0500

    created Person, Instructor and part of Person classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,37 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(instructorAtt){
+        super(instructorAtt);
+        this.specialty = instructorAtt.specialty;
+        this.favLanguage = instructorAtt.favLanguage;
+        this.catchPhrase =instructorAtt.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`)
+    }
+    grade(student, subject) {
+        console.log(`${student} receives a perfect score on ${subject}`)
+    }
+}
+
+class Student extends Person{
+    constructor(studentAttr){
+        super(studentAttr);
+        this.previousBackground = studentAttr.previousBackground;
+        this.className = studentAttr.className;
+        this.favSubjects = studentAttr.className;
+    }
+
+}
